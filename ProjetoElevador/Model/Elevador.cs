2cacec5a61debd598f53a07412517a41ff4ff07f
Mateus\mathc <math.cost.oliv.incomplete@gmail.com>
2022-05-29 09:54:25 -0300

    commit 1

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    public class Elevador
+    {
+        
+        public int AndarAtual { get; set; } //andar atual
+        public int TotalDeAndares { get; set; } //total de andares
+        public int CapacidadeDoElevador { get; set; } //capacidade max do elevador
+        public int QuantidadeDePessoas { get; set; } //quantidade max de pessoas
+
+        public void Inicializar(int totalDeAndares, int capacidadeDoElevador)
+        {
+            TotalDeAndares = totalDeAndares;
+            CapacidadeDoElevador = capacidadeDoElevador;
+        }
+        public void Entrar()
+        {
+            if (this.QuantidadeDePessoas < this.CapacidadeDoElevador)
+            {
+                this.QuantidadeDePessoas++;
+            }
+        }
+        public void Sair()
+        {
+            if (this.QuantidadeDePessoas > 0)
+            {
+                this.QuantidadeDePessoas--;
+            }
+        }
+        public void Subir()
+        {
+            if (this.AndarAtual < TotalDeAndares)
+            {
+                this.AndarAtual++;
+            }
+        }
+        public void Descer()
+        {
+            if (this.AndarAtual > 0)
+            {
+                this.AndarAtual--;
+            }
+        }
+
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,78 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //o programa espera o usuario digitar
+            Console.WriteLine(@"Escolha uma opcao:
+                                1 - Inicializar
+
+               
+             ");
+            var Opcao1 = Console.ReadLine(); // o programa le a opcao digitada pelo usuario
+            if (Opcao1 == "1")
+            {
+
+                Elevador p = new Elevador();
+                Console.WriteLine("Voce escolheu a opcao 1");
+
+                //usuario vai escolher o andar e a capacidade e pessoas
+
+                Console.WriteLine("Digite a quantidade total de andares");
+                p.TotalDeAndares = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a capacidade total do elevador");
+                p.CapacidadeDoElevador = int.Parse(Console.ReadLine());
+                while (p != null)
+                {
+                    Console.WriteLine(@"Escolha uma opcao:
+                                2 - Entrar
+                                3 - Sair
+                                4 - Subir
+                                5 - Descer
+                       ");
+                    string OpcaoEscolhida2 = Console.ReadLine();
+
+                    switch (OpcaoEscolhida2)
+                    {
+                        case "2":
+                            p.Entrar();
+                            Console.WriteLine("Entrar");
+                            Console.WriteLine($"Total de Pessoas {p.QuantidadeDePessoas}");
+                            Console.WriteLine($"Andar {p.AndarAtual}");
+                            break;
+                        case "3":
+                            p.Sair();
+                            Console.WriteLine("Sair");
+                            Console.WriteLine($"Total de Pessoas {p.QuantidadeDePessoas}");
+                            Console.WriteLine($"Andar {p.AndarAtual}");
+                            break;
+                        case "4":
+                            p.Subir();
+                            Console.WriteLine("Subir");
+                            Console.WriteLine($"Total de Pessoas {p.QuantidadeDePessoas}");
+                            Console.WriteLine($"Andar {p.AndarAtual}");
+                            break;
+                        case "5":
+                            p.Descer();
+                            Console.WriteLine("Descer");
+                            Console.WriteLine($"Total de Pessoas {p.QuantidadeDePessoas}");
+                            Console.WriteLine($"Andar {p.AndarAtual}");
+                            break;
+                        default:
+                            Console.WriteLine("Opcao Invalida");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opcao Invalida");
+            }
+
+        }
+    }
+}
